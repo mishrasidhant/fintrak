6baeb7e892df37c77bff082a7b94baea5f05d933
mishrasidhant <31853317+mishrasidhant@users.noreply.github.com>
2025-06-10 08:29:51 -0400

    Gather account data sequentially to retain context

--- attach.js
+++ attach.js
@@ -1,49 +1,56 @@
-import puppeteer from 'puppeteer'
-import {writeFile} from 'node:fs'
-import 'dotenv/config'
+import puppeteer from "puppeteer";
+import {
+	writeFile
+} from "node:fs";
+import "dotenv/config";
 
 const browserWSEndpoint = process.argv[2];
 
-function delay(ms){
-	return new Promise(resolve => setTimeout(resolve, ms));
+function delay(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function debugFile(file, content){
-	writeFile(file, content, 'utf8', (err) => {
+function debugFile(file, content) {
+	writeFile(file, content, "utf8", (err) => {
 		if (err) throw err;
-		console.log(`Successfully wrote file :${file}`)
+		console.log(`Successfully wrote file :${file}`);
 	});
 }
 
 const connectToPages = (async (browserWSEndpoint, cleanup) => {
-	console.log(browserWSEndpoint)
+	console.log(browserWSEndpoint);
 	const browser = await puppeteer.connect({
 		browserWSEndpoint: browserWSEndpoint,
-		defaultViewport: {width: 1280, height: 999},
-	})
+		defaultViewport: {
+			width: 1280,
+			height: 999,
+		},
+	});
 
 	const pages = await browser.pages();
-	console.log("Open pages: ", pages.map(p => p.url()));
+	console.log(
+		"Open pages: ",
+		pages.map((p) => p.url())
+	);
 
-	// Filter simplii page and navigate to login page 
-	const simpliiPage = pages.filter(p =>  p.url().includes(process.env.SIMPLII_URL_INCLUDES))
-	const page = simpliiPage[0]
+	// Filter simplii page and navigate to login page
+	const simpliiPage = pages.filter((p) =>
+		p.url().includes(process.env.SIMPLII_URL_INCLUDES)
+	);
+	const page = simpliiPage[0];
 
-	const statementLink = await page.locator('[data-test-id="olb-extras-view-estatements"]')
-	await statementLink.scroll()
-	delay(10000)
-	await statementLink.click()
+	const homeLink = await page.locator("a ::-p-text(Home)");
+	await homeLink.click();
+
+	/* Navigate to estatements page */
+	const statementLink = await page.locator(
+		'[data-test-id="olb-extras-view-estatements"]'
+	);
+	await statementLink.scroll();
+	await statementLink.click();
 	await page.waitForNetworkIdle();
-	console.log('network idle complete')
 
-	// #ember1232 > div.online-statements > div.account-selector-section > div.account-list
-	// #ember1247 : ui-select
-	// #ember1247-field : select
-	// #ember1253 : option
-	// div.account-list ui-select
-	// const options = await page.$$('#ember1246-field option');
-
-/*
+	/*
 	// THIS WORKS!! -> but promise.All is more performant -> although I like the readability here
 	const options = await page.$$('#ember1247-field option');
 	console.log(`found ${options.length} options`)
@@ -58,18 +65,149 @@ const connectToPages = (async (browserWSEndpoint, cleanup) => {
 	console.log(optionsList)
 */
 
-	const optionsList = await page.$$eval('#ember1247-field option', async opts => 
-		Promise.all(opts.map(async opt => ({
-			value: opt.value,
-			text: opt.textContent.trim(),
-		})))
+	/* Get acc list */
+	const accList = await page.$$eval(
+		"div.online-statements div.account-selector-section ui-select select option",
+		async (opts) =>
+			Promise.all(
+				// Get all accounts
+				opts.map(async (opt) => ({
+					selectValue: opt.value,
+					text: opt.textContent.trim(),
+				}))
+			)
 	);
-	console.log(optionsList)
 
-	delay(10000)
+	/* Filter acc list and add account numbers */
+	const cfgAccList = process.env.SIMPLII_ACC_LIST.split(",");
+	const filteredAccList = accList
+		.filter((acc) => cfgAccList.some((cfgAcc) => acc.text.includes(cfgAcc)))
+		.map((acc) => {
+			const account = cfgAccList.find((cfgAcc) => acc.text.includes(cfgAcc));
+			const formattedAccount = account.split(" ").join("_");
+			return {
+				...acc,
+				account,
+				formattedAccount
+			};
+		});
+	console.log(filteredAccList);
 
+	const startYear = parseInt(process.env.SIMPLII_START_YEAR || "2018", 10);
+	const endYear = new Date().getFullYear();
+	const validYears = Array.from({
+			length: endYear - startYear + 1
+		}, (_, i) =>
+		(startYear + i).toString()
+	);
 
-	await browser.disconnect()
-})(browserWSEndpoint, () => { console.log('cleanup invoked)')})
+	/* Act on individual Accounts */
+	const bankData = [];
+	const getAccountData = async function (acc) {
+		await page.select(
+			"div.online-statements div.account-selector-section ui-select select",
+			`${acc.selectValue}`
+		);
+		await page.waitForNetworkIdle();
+		const accData = await page.$$eval(
+			'span[role="button"]',
+			(accSpans, validYears, account, formattedAccount, selectValue) => {
+				return accSpans
+					.filter((span) =>
+						validYears.some((year) => span.innerText.includes(year))
+					)
+					.map((span) => {
+						const spanText = span.innerText.trim();
+						const year = validYears.find((validYear) =>
+							spanText.includes(validYear)
+						);
+						return {
+							spanId: span.getAttribute("id") || null,
+							selectValue,
+							year: year || null,
+							text: spanText,
+							account: account,
+							formattedAccount: formattedAccount,
+						};
+					});
+			},
+			validYears,
+			acc.account,
+			acc.formattedAccount,
+			acc.selectValue
+		);
+		return accData;
+	};
+	// TODO Document
+	const getStatementList = async function (acc) {
+		for (const yearData of acc) {
+			const span = await page.$(`#${yearData.spanId}`);
+			const spanParentDiv = await span.evaluateHandle(
+				(span) => span.parentElement
+			);
+			const parentDivContent = await spanParentDiv.evaluateHandle(
+				(div) => div.nextElementSibling
+			);
+			const ul = await parentDivContent.$("ul.monthly-statements");
+			const liList = await ul.$$("li");
 
-await connectToPages
+			const buttonsData = [];
+			for (const li of liList) {
+				const uiButton = await li.$("ui-button");
+				if (!uiButton) continue;
+
+				const uiButtonId = await uiButton.evaluate((el) =>
+					el.getAttribute("id")
+				);
+				const uiButtonAriaLabel = await uiButton.evaluate((el) =>
+					el.getAttribute("aria-label")
+				);
+
+				const buttonHandle = await uiButton.$("button");
+				const buttonText = buttonHandle ?
+					await buttonHandle.evaluate((el) => el.textContent.trim()) :
+					"";
+
+				buttonsData.push({
+					clickableId: uiButtonId,
+					ariaLabel: uiButtonAriaLabel,
+					buttonText,
+				});
+			}
+			yearData.statementData = buttonsData;
+		}
+	};
+	for (const acc of filteredAccList) {
+		const accData = await getAccountData(acc);
+		// Process account specific data here!
+		await getStatementList(accData);
+		console.log(accData[0]);
+		bankData.push(accData);
+	}
+
+	/*
+	for (const accData of bankData){
+		for (const yearData of accData){
+			const selector = `#${yearData.spanId}`
+			console.log(selector)
+			const span = await page.$(selector);
+			console.log(span)
+			// const spanParent = await span.evaluateHandle(span => span.parentElement)
+			// console.log(spanParent)
+			// const contentDiv = await spanParent.evaluateHandle(div => div.nextElementSibling)
+			// console.log(contentDiv)
+		}
+	}
+*/
+
+	/* Attach an array of monthly statement buttons to bank metadata
+	
+	*/
+	// await delay(5000)
+
+	await browser.disconnect();
+})(browserWSEndpoint, () => {
+	console.log("cleanup invoked)");
+});
+
+await connectToPages;
